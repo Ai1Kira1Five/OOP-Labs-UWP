857639b1b1fe837ce416056e8b040fd04e6a2437
Artyom <artemius16071998@gmail.com>
2019-03-13 15:48:03 +0300

    Start 2 lab

--- OOP_Labs_UWP/LabPage2.xaml.cs
+++ OOP_Labs_UWP/LabPage2.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +26,77 @@ namespace OOP_Labs_UWP
         public LabPage2()
         {
             this.InitializeComponent();
+        }
+
+        private string typeOutput;
+
+        private void onPageCanvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            string tp = "ms-appx:///Assets/";
+            var x = e.GetCurrentPoint(onPageCanvas).Position.X;
+            var y = e.GetCurrentPoint(onPageCanvas).Position.Y;
+
+            var bh = ((Canvas)sender).Height;
+            var bw = ((Canvas)sender).Width;
+
+            Image newImage = new Image();
+
+            switch (typeOutput)
+            {
+                case "man":
+                    tp += "man.png";
+                    newImage.Height = 120;
+                    newImage.Width = 120;
+                    break;
+                case "school":
+                    tp += "school.jpg";
+                    newImage.Height = 120;
+                    newImage.Width = 120;
+                    break;
+                case "student":
+                    tp += "idiot.gif";
+                    newImage.Height = 100;
+                    newImage.Width = newImage.Height * 1.78;
+                    break;
+                case "zaoch":
+                    tp += "zaoch.gif";
+                    newImage.Height = 100;
+                    newImage.Width = newImage.Height * 1.33;
+                    break;
+                default:
+                    tp += "test.png";
+                    newImage.Height = 48;
+                    newImage.Width = 48;
+                    break;
+            }
+
+            BitmapImage image = new BitmapImage(new Uri(tp));
+            newImage.Source = image;
+
+            newImage.Margin = (bw - newImage.Width) > y ? new Thickness(x - newImage.Width, y, 0, 0) : new Thickness(x, y, 0, 0);
+
+            onPageCanvas.Children.Add(newImage);
+        }
+
+        private void Choose_Click(object sender, RoutedEventArgs e)
+        {
+            string type = ((Button)sender).Name;
+
+            switch (type)
+            {
+                case "Man":
+                    typeOutput = "man";
+                    break;
+                case "School":
+                    typeOutput = "school";
+                    break;
+                case "Student":
+                    typeOutput = "student";
+                    break;
+                case "Zaoch":
+                    typeOutput = "zaoch";
+                    break;
+            }            
         }
     }
 }
